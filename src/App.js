b7e558013c1bf7edd367a1c0809ce1979135a29a
AnthonyFrederick7 <119832448+AnthonyFrederick7@users.noreply.github.com>
2023-03-09 09:43:17 -0600

    U

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import './App.css';
-import HomePage from './pages/home/index.js';
+import HomePage from './pages';
 
 function App() {
 
   return (
-        <HomePage />
+    <Router>
+      <div style={{ backgroundImage: "linear-gradient(to right, #18503c 0%, #1d2e4e 100%)" }}>
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
